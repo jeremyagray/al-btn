8bdfb210518cc95b087d911e9b9176e79042c2fe
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-24 05:54:10 -0500

    feat(react): add app header component
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+// import {
+//   useState
+// } from 'react';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="AppHeader">
+    </div>
+  );
+}
+
+export default Header;
