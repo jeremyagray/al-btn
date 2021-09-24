ea503bbd33e0470bc223d1189f8ca81d862fe4e0
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-24 13:46:39 -0500

    feat(react): add app body component
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/App.js
+++ src/App.js
@@ -11,11 +11,15 @@
 import './App.css';
 
 import Header from './Header.js';
+import Body from './Body.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/Body.js
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+// import {
+//   useState
+// } from 'react';
+
+import './Body.css';
+
+function Body() {
+  return (
+    <div className="AppBody">
+      <p>
+        Body container.
+      </p>
+    </div>
+  );
+}
+
+export default Body;
