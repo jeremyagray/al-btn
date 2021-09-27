7c792e3846320769ae04ef459a0ceaae75579b19
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-27 05:43:33 -0500

    feat(react): add basic styling
    Add basic styling to the header and footer.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -8,15 +8,22 @@
 //   useState
 // } from 'react';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
 import './Header.css';
 
 function Header() {
   return (
-    <div className="AppHeader">
-      <h1>
-        Alabama:  By the Numbers
-      </h1>
-    </div>
+    <header className="AppHeader">
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#">
+            Alabama:  By the Numbers
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
