66750d300ca653237fe544e8f233684b9549c7e5
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-29 12:43:38 -0500

    feat(react): add dummy contact component
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="AppContact">
+    </div>
+  );
+}
+
+export default Contact;
