f7e17cfa2cb280cb30b043f665fc29758250a3fc
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-29 05:48:13 -0500

    feat(react): add navbar and styling
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,8 @@
 // } from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 
 import './Header.css';
@@ -16,11 +18,32 @@ import './Header.css';
 function Header() {
   return (
     <header className="AppHeader">
-      <Navbar>
+      <Navbar
+        expand="lg"
+      >
         <Container>
-          <Navbar.Brand href="#">
+          <Navbar.Brand href="#" className="d-none d-sm-block">
             Alabama:  By the Numbers
           </Navbar.Brand>
+          <Navbar.Brand href="#" className="d-small d-md-none">
+            AL:  BTN
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#contact">
+                Contact
+              </Nav.Link>
+              <NavDropdown title="Account" id="albtn-navbar-account-dropdown">
+                <NavDropdown.Item href="#register">
+                  Register
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#apitoken">
+                  API Token
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
