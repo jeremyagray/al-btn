ff7c568e5a717245d243374a207f9743be47ec8d
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-30 15:20:36 -0500

    feat(react): add stub registration component
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/Register.js
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+import './Register.css';
+
+function Register() {
+  return (
+    <div className="AppRegister">
+      <p>
+        Registration component
+      </p>
+    </div>
+  );
+}
+
+export default Register;
