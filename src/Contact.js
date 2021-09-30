dbfb1c1f548e24169793ea6798a2a1cda5b6ec4e
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-29 20:58:37 -0500

    feat(react): complete basic contact component
    Complete basic contact component and add anti-spam field.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Contact.js
+++ src/Contact.js
@@ -4,11 +4,48 @@
  * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
  */
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './Contact.css';
 
 function Contact() {
   return (
     <div className="AppContact">
+      <h2>
+        Contact
+      </h2>
+      <p className="text-muted">
+        All information entered here will not be sold or used for purposes other than the use of this site.  Thanks!
+      </p>
+      <Form className="p-3">
+        <Form.Group className="p-3 mb-3" controlId="contactFormName">
+          <Form.Label>
+            Name
+          </Form.Label>
+          <Form.Control required placeholder="enter your name" />
+        </Form.Group>
+
+        <Form.Group className="p-3 mb-3" controlId="contactFormEmail">
+          <Form.Label>
+            Email
+          </Form.Label>
+          <Form.Control required type="email" placeholder="enter email" />
+        </Form.Group>
+
+        <Form.Group className="p-3 mb-3" controlId="contactFormMessage">
+          <Form.Label>
+            Message
+          </Form.Label>
+          <Form.Control as="textarea" rows={3} required placeholder="enter your message" />
+        </Form.Group>
+
+        <Form.Control hidden placeholder="enter a value if you are a spammer" />
+
+        <Button id="contactFormSubmit" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
