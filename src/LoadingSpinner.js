17f58235cb11f255670042531689016ea229d246
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-06 09:41:10 -0500

    refactor(react): create loading spinner component
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/LoadingSpinner.js
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+// React Bootstrap.
+import Spinner from 'react-bootstrap/Spinner';
+
+export const LoadingSpinner = (props) => {
+  return (
+    <div
+      id="viz"
+      ref={props.vizRef}
+      className="d-flex"
+    >
+      <Spinner
+        animation="border"
+        role="status"
+        className="mx-auto my-auto"
+      >
+        <span className="visually-hidden">
+          Loading data, please be patient.
+        </span>
+      </Spinner>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
