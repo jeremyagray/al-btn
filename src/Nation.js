292b99c9a97ef13295f55d703053080e1f2c4b21
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-08 13:01:03 -0500

    feat(react): convert map generation to react
    Convert map generation to a react component instead of a D3 function,
    fixing ResizeObserver problems.  Refactor data fetching and rendering
    components to separate concerns.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Nation.js
+++ src/Nation.js
@@ -4,37 +4,17 @@
  * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
  */
 
-// React.
-import {
-  useEffect,
-  useState
-} from 'react';
-
 // D3.
 import * as d3 from 'd3';
 import { geoAlbersUsaTerritories } from 'd3-composite-projections';
 
-// Other stuff.
-import axios from 'axios';
-
 // Local components.
 import useChartDimensions from './useChartDimensions';
+import useFetchData from './useFetchData';
 import LoadingSpinner from './LoadingSpinner';
 import LoadingError from './LoadingError';
 
 export const NationMap = (props) => {
-  return (
-    <div className="NationMap">
-      <NationMapSVG
-        projection={props.projection}
-        showStates={props.showStates}
-      />
-    </div>
-  );
-}
-
-const NationMapSVG = (props) => {
-  // Dimensions.
   const [ref, dms] = useChartDimensions({
     'marginTop': 30,
     'marginRight': 30,
@@ -42,136 +22,82 @@ const NationMapSVG = (props) => {
     'marginLeft': 30
   });
 
-  // Data state.
-  const [mapData, setMapData] = useState({'type': ''});
-  const [stateData, setStateData] = useState({'type': ''});
-  const [loadingData, setLoadingData] = useState(true);
-  const [loadingDataError, setLoadingDataError] = useState(null);
+  const [
+    nationData,
+    nationLoadingData,
+    nationLoadingDataError
+  ] = useFetchData('http://192.168.1.67:3002/api/v1/geography/nation');
 
-  useEffect(() => {
-    const mapDataURL = 'http://192.168.1.67:3002/api/v1/geography/nation';
-    const stateDataURL = 'http://192.168.1.67:3002/api/v1/geography/states/all';
-
-    let isMounted = true;
-    setLoadingData(true);
-
-    async function fetchData() {
-      try {
-        const mapResponse = await axios.get(mapDataURL);
-        let stateResponse;
-        if (props.showStates) {
-          stateResponse = await axios.get(stateDataURL);
-        }
-
-        if (isMounted) {
-          setMapData(mapResponse.data);
-          if (props.showStates) {
-            setStateData(stateResponse.data);
-          }
-          setLoadingData(false);
-          if (props.showStates) {
-            generateMap(mapData, stateData, props.projection, ref.current, dms);
-          } else {
-            generateMap(mapData, [], props.projection, ref.current, dms);
-          }
-        }
-      } catch (error) {
-        if (isMounted) {
-          setLoadingDataError(error);
-          setLoadingData(false);
-        }
-      }
+  if (nationLoadingData) {
+    return (
+      <div
+        className="visualization d-flex"
+        ref={ref}
+      >
+        <LoadingSpinner />
+      </div>
+    );
+  } else if (nationLoadingDataError) {
+    return (
+      <div
+        className="visualization"
+        ref={ref}
+      >
+        <LoadingError
+          errorMessage={nationLoadingDataError.message}
+        />
+      </div>
+    );
+  } else {
+    // Map projection.
+    let projection;
+    if (props.projection === 'nation50') {
+      projection = d3.geoAlbersUsa()
+        .fitSize([dms.boundedWidth, dms.boundedHeight], nationData);
+    } else if (props.projection === 'nationAll') {
+      projection = geoAlbersUsaTerritories()
+        .fitSize([dms.boundedWidth, dms.boundedHeight], nationData);
+    } else if (props.projection === 'nationSingle') {
+      projection = d3.geoAlbers()
+        .fitSize([dms.boundedWidth, dms.boundedHeight], nationData);
     }
 
-    fetchData();
+    // Path generator.
+    const path = d3.geoPath().projection(projection);
 
-    return () => {
-      isMounted = false;
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mapData['type'], stateData['type'], dms.boundedWidth, props.projection, props.showStates]);
-
-  if (loadingData) {
     return (
-      <LoadingSpinner vizRef={ref} />
+      <div
+        className="visualization"
+        ref={ref}
+      >
+        <svg
+          id="mapSVG"
+          height={dms.height}
+          width={dms.width}
+          style={{
+            'backgroundColor': '#ffffff'
+          }}
+        >
+        {nationData.features.map((feat) => {
+          return (
+            <path
+              className="nation"
+              key={feat.properties.GEOID}
+              stroke="#000000"
+              strokeLinejoin="round"
+              d={path(feat)}
+              transform={`translate(${dms.marginLeft}, ${dms.marginTop})`}
+              style={{
+                'fill': '#b20021'
+              }}
+            >
+            </path>
+          );
+        })}
+        </svg>
+      </div>
     );
-  } else if (loadingDataError) {
-    return (
-      <LoadingError
-        vizRef={ref}
-        errorMessage={loadingDataError.message}
-      />
-    );
   }
-
-  return (
-    <div
-      id="viz"
-      ref={ref}
-    />
-  );
-}
-
-const generateMap = (mapData, stateData, proj, element, dimensions) => {
-
-  // Map projection.
-  let projection;
-  if (proj === 'nation50') {
-    projection = d3.geoAlbersUsa()
-      .fitSize([dimensions.boundedWidth, dimensions.boundedHeight], mapData);
-  } else if (proj === 'nationAll') {
-    projection = geoAlbersUsaTerritories()
-      .fitSize([dimensions.boundedWidth, dimensions.boundedHeight], mapData);
-  } else if (proj === 'nationSingle') {
-    projection = d3.geoAlbers()
-      .fitSize([dimensions.boundedWidth, dimensions.boundedHeight], mapData);
-  }
-
-  // Path generator.
-  const path = d3.geoPath()
-        .projection(projection);
-
-  // Visualization SVG.
-  const svg = d3.select(element)
-    .append('svg')
-    .attr("id", "mapSVG")
-    .attr("height", dimensions.height)
-    .attr("width", dimensions.width)
-    .style('background-color', '#ffffff')
-    .style('display', 'none')
-    .style('visibility', 'hidden');
-
-  // Draw national boundaries.
-  svg
-    .selectAll('path')
-    .data(mapData.features)
-    .join('path')
-    .attr('class', 'nation')
-    .style('fill', '#b20021')
-    .attr('stroke', '#000000')
-    .attr('stroke-linejoin', 'round')
-    .attr('d', path)
-    .attr('transform',
-          `translate(${dimensions.marginLeft}, ${dimensions.marginTop})`);
-
-  // Draw state boundaries.
-  if (stateData.features) {
-    svg
-      .selectAll('path')
-      .data(stateData.features)
-      .join('path')
-      .attr('class', 'state')
-      .style('fill', '#b20021')
-      .attr('stroke', '#000000')
-      .attr('stroke-linejoin', 'round')
-      .attr('d', path)
-      .attr('transform',
-            `translate(${dimensions.marginLeft}, ${dimensions.marginTop})`);
-  }
-
-  svg
-    .style('display', 'block')
-    .style('visibility', 'visible')
 }
 
 export default NationMap;
