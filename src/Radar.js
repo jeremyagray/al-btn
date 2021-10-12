d1fddcd6ed16fbc4d815bd2f76c0458dbde2db89
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-11 22:07:39 -0500

    fix(data): fix radar image size bug
    Set radar image size to bounded dimensions in radar API call.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Radar.js
+++ src/Radar.js
@@ -15,7 +15,7 @@ const Radar = (props) => {
     const [miny, maxx] = props.projection.invert([0, 0]);
     const [maxy, minx] = props.projection.invert([props.dms.boundedWidth, props.dms.boundedHeight]);
 
-    radarDataUrl = `https://opengeo.ncep.noaa.gov/geoserver/kbmx/ows?service=wms&version=1.3.0&request=GetMap&format=image/png&&layers=kbmx_bref_raw&crs=EPSG:4326&transparent=true&width=${Math.floor(props.dms.width)}&height=${Math.floor(props.dms.height)}&bbox=${minx},${miny},${maxx},${maxy}`;
+    radarDataUrl = `https://opengeo.ncep.noaa.gov/geoserver/kbmx/ows?service=wms&version=1.3.0&request=GetMap&format=image/png&&layers=kbmx_bref_raw&crs=EPSG:4326&transparent=true&width=${Math.floor(props.dms.boundedWidth)}&height=${Math.floor(props.dms.boundedHeight)}&bbox=${minx},${miny},${maxx},${maxy}`;
   }
 
   const [
