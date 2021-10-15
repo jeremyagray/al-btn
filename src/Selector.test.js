2f5bb85316139c0746124013b63d2d6eead5aecd
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-15 13:03:34 -0500

    test(react): add select update test
    Test if select updates visualization.  Add and pass mock props for
    tests.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Selector.test.js
+++ src/Selector.test.js
@@ -5,16 +5,45 @@
  */
 
 import {
+  fireEvent,
   render,
   screen
 } from '@testing-library/react';
 
 import Selector from './Selector';
 
+let props = {
+  'visualization': 'national',
+  'visualizations': [
+    {
+      'value': 'national',
+      'key': 'national',
+      'title': 'national map',
+    },
+    {
+      'value': 'states',
+      'key': 'states',
+      'title': 'state map',
+    }
+  ],
+  'updateVisualization': (event) => {
+    props.visualization = event.target.value;
+  }
+};
+
 test('renders selector', () => {
-  render(<Selector />);
+  render(<Selector {...props} />);
+  
   expect(
     screen
       .getByLabelText('Select Visualization'))
     .toBeInTheDocument();
+});
+
+test('selector changes visualization', () => {
+  render(<Selector {...props} />);
+  
+  expect(screen.getByLabelText('Select Visualization')).toBeInTheDocument();
+  fireEvent.change(screen.getByLabelText('Select Visualization'), {'target': {'value': 'states' }});
+  expect(props.visualization).toBe('states');
 });
