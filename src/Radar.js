627d77e784eb4c478164c5888f5a651c7e6fb9fd
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-16 21:08:19 -0500

    feat(react): implement basic radar refetching
    Implement basic radar data refetching to match latency of KBMX radar.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Radar.js
+++ src/Radar.js
@@ -4,7 +4,7 @@
  * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
  */
 
-import useFetchData from './useFetchData';
+import useRefetchData from './useRefetchData';
 
 import './Radar.css';
 
@@ -16,11 +16,12 @@ const Radar = (props) => {
     const [maxy, minx] = props.projection.invert([props.dms.boundedWidth, props.dms.boundedHeight]);
 
     radarDataUrl = `https://opengeo.ncep.noaa.gov/geoserver/kbmx/ows?service=wms&version=1.3.0&request=GetMap&format=image/png&&layers=kbmx_bref_raw&crs=EPSG:4326&transparent=true&width=${Math.floor(props.dms.boundedWidth)}&height=${Math.floor(props.dms.boundedHeight)}&bbox=${minx},${miny},${maxx},${maxy}`;
+    console.log(radarDataUrl);
   }
 
   const [
     radarData,
-  ] = useFetchData(radarDataUrl, null, {'responseType': 'blob'});
+  ] = useRefetchData(radarDataUrl, 120000, null, {'responseType': 'blob'});
 
   if (props.showRadar && radarData) {
     if (props.clipToState) {
---
+++ src/useRefetchData.js
@@ -0,0 +1,62 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+// React.
+import {
+  useEffect,
+  useState
+} from 'react';
+
+// Other stuff.
+import axios from 'axios';
+
+export const useRefetchData = (url, interval, dataDefault = null, config = {}) => {
+  // Data state.
+  const [data, setData] = useState(dataDefault);
+  const [loadingData, setLoadingData] = useState(true);
+  const [loadingDataError, setLoadingDataError] = useState(null);
+
+  useEffect(() => {
+    // let timer;
+    let isMounted = true;
+    setLoadingData(true);
+
+    const fetchData = async () => {
+      try {
+        let response = {};
+
+        if (! url) {
+          response.data = dataDefault;
+        } else {
+          response = await axios.get(url, config);
+        }
+
+        if (isMounted) {
+          setData(response.data);
+          setLoadingData(false);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setLoadingDataError(error);
+          setLoadingData(false);
+        }
+      }
+    }
+
+    fetchData();
+    const id = setInterval(fetchData, 120000);
+
+    return () => {
+      isMounted = false;
+      clearInterval(id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
+  return [ data, loadingData, loadingDataError ];
+}
+
+export default useRefetchData;
