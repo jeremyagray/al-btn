f484cc0c5112b1dea00d84ee00c03739da655527
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-21 12:13:53 -0500

    test(react): add `GeographyConfig` component tests
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/GeographyConfig.test.js
@@ -0,0 +1,36 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+
+import GeographyConfig from './GeographyConfig.js';
+
+test('renders the component', async () => {
+  let props = {
+    'currentState': 'AL',
+    'updateCurrentState': jest.fn(),
+    'clipToState': true,
+    'toggleClipToState': jest.fn(),
+    'showSurroundingStates': true,
+    'toggleShowSurroundingStates': jest.fn(),
+    'showCounties': true,
+    'toggleShowCounties': jest.fn()
+  };
+
+  render(<GeographyConfig {...props} />);
+  
+  let ele = await screen.findByText(/Select State\/Territory/i);
+  expect(ele).toBeInTheDocument();
+  ele = await screen.findByText(/clip to state/i);
+  expect(ele).toBeInTheDocument();
+  ele = await screen.findByText(/show surrounding states/i);
+  expect(ele).toBeInTheDocument();
+  ele = await screen.findByText(/show counties/i);
+  expect(ele).toBeInTheDocument();
+});
