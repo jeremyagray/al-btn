c8c3dba2181f225cb22e69eede51306d1e93d31d
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-23 16:46:34 -0500

    feat(react): add basic form handling functions
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/Contact.js
+++ src/Contact.js
@@ -4,12 +4,47 @@
  * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
  */
 
+// React.
+import {
+  useState
+} from 'react';
+
+// React Bootstrap.
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import './Contact.css';
 
 function Contact() {
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ message, setMessage ] = useState('');
+  const [ antispam, setAntispam ] = useState('');
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessage = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleAntispam = (event) => {
+    setAntispam(event.target.value);
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(`Name:  ${name}`);
+    console.log(`Email:  ${email}`);
+    console.log(`Message:  ${message}`);
+    console.log(`Antispam:  ${antispam}`);
+  };
+
   return (
     <div className="AppContact p-3">
       <h2>
@@ -18,31 +53,61 @@ function Contact() {
       <p className="text-muted">
         All information entered here will not be sold or used for purposes other than the use of this site.  Thanks!
       </p>
-      <Form className="p-3">
+      <Form
+        className="p-3"
+        onSubmit={submitForm}
+      >
         <Form.Group className="p-3 mb-3" controlId="contactFormName">
           <Form.Label>
             Name
           </Form.Label>
-          <Form.Control required placeholder="enter your name" />
+          <Form.Control
+            required
+            placeholder="enter your name"
+            value={name}
+            onChange={handleName}
+          />
         </Form.Group>
 
         <Form.Group className="p-3 mb-3" controlId="contactFormEmail">
           <Form.Label>
             Email
           </Form.Label>
-          <Form.Control required type="email" placeholder="enter email" />
+          <Form.Control
+            required
+            type="email"
+            placeholder="enter email"
+            value={email}
+            onChange={handleEmail}
+          />
         </Form.Group>
 
         <Form.Group className="p-3 mb-3" controlId="contactFormMessage">
           <Form.Label>
             Message
           </Form.Label>
-          <Form.Control as="textarea" rows={3} required placeholder="enter your message" />
+          <Form.Control
+            required
+            as="textarea"
+            rows={3}
+            placeholder="enter your message"
+            value={message}
+            onChange={handleMessage}
+          />
         </Form.Group>
 
-        <Form.Control hidden placeholder="enter a value if you are a spammer" />
+        <Form.Control
+          hidden
+          placeholder="enter a value if you are a spammer"
+          value={antispam}
+          onChange={handleAntispam}
+        />
 
-        <Button id="contactFormSubmit" variant="primary" type="submit">
+        <Button
+          id="contactFormSubmit"
+          variant="primary"
+          type="submit"
+        >
           Submit
         </Button>
       </Form>
